b909f8474372270b1e42b56ef463a715c52fbfd1
takatogoto <42685217+takatogoto@users.noreply.github.com>
2018-12-29 10:37:11 -0800

    everyone

---
+++ scopetest1.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+def foo():
+    b = 10
+    print(a,b)
+
+foo()
+print(a,b)
---
+++ testmodule.py
@@ -0,0 +1,12 @@
+import sys
+
+a = 1
+b = "some string"
+
+def foo():
+    print("This is the function 'foo'")
+    
+print("This is the top level")
+
+if __name__ == '__main__':
+    print("This is the code block")
